c95487f770987b471e9c33eace13aca18ca03441
Augusto camati <auustocamati21@gmail.com>
2025-04-01 17:13:15 +0100

    removendo código comentado da tabela de permissões

--- src/app/permissoes/page.tsx
+++ src/app/permissoes/page.tsx
@@ -652,9 +652,9 @@ export default function Permissoes() {
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                           Salas
                         </th>
-                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        {/* <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                           Status
-                        </th>
+                        </th> */}
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                           Ações
                         </th>
